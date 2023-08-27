5cc743ed3d1e5c3f440a5f617edd2b19f2a120fd
Saugat Paudel <lpsaugat@gmail.com>
2023-08-27 20:57:39 +0545

    Update: Use dependencies

---
+++ config/config.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+require("colors");
+
+//Connect DB
+const ConnectDB = async () => {
+  try {
+  } catch {}
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+//dotenv
+const dotenv = require("dotenv");
+dotenv.config();
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("default"));
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("<h1>POS BACKEND</h1>");
+});
+
+//PORT
+const PORT = process.env.PORT || 2000;
+
+//listen
+app.listen(PORT, () => {
+  console.log(`Server is Running at PORT: ${PORT}`);
+});
